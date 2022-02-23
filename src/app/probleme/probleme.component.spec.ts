84b691739b8551dab04c79eb44e9158f03c63181
Mehdi Zergoug <mehdizer15@gmail.com>
2022-02-23 16:41:47 -0500

    TP7pt1

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProblemeComponent } from './probleme.component';
 
@@ -8,6 +9,7 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ ProblemeComponent ]
     })
     .compileComponents();
@@ -19,7 +21,42 @@ describe('ProblemeComponent', () => {
     fixture.detectChanges();
   });
 
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+
   it('should create', () => {
-    expect(component).toBeTruthy();
+  expect(component).toBeTruthy();
   });
+
+  it("#1 | Zone PRÉNOM invalide avec 2 caractères", () => {
+    // let errors = {};
+    // let zone = component.problemeForm.get('prenom');
+    // zone.setValue("a".repeat(3));
+    // errors = zone.errors || {};
+    // expect(errors['minLength']).toBeTruthy();
+
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("a".repeat(2));
+    let errors = zone.errors || {};
+    expect(errors['minlength']).toBeTruthy();
+  });
+
+  it("#1 | Zone PRÉNOM valide avec 3 caractère", () => {
+
+
+  });
+
+  // it("#3 | Zone PRÉNOM valide avec 3 caractère", () => {
+  // });
+
+  it("#4 | Zone PRÉNOM invalide avec aucune valeur", () => {
+  });
+
+  it("#5 | Zone PRÉNOM valide avec 10 espace", () => {
+  });
+
+  it("#6 | Zone PRÉNOM valide avec 2 espace et 1 caractère", () => {
+  });
+
 });
--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'inter-probleme',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./probleme.component.css']
 })
 export class ProblemeComponent implements OnInit {
+  save(): void {
+  }
+  
+  problemeForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.problemeForm = this.fb.group({
+      prenom: ['', [Validators.minLength(3)]],
+    })
   }
 
 }
