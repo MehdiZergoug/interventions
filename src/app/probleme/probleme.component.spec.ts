c2a7b210d5f94813a714ae18e57abca563ff8c83
Mehdi Zergoug <mehdizer15@gmail.com>
2022-02-23 17:39:06 -0500

    TP7pt2

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -42,21 +42,35 @@ describe('ProblemeComponent', () => {
     expect(errors['minlength']).toBeTruthy();
   });
 
-  it("#1 | Zone PRÉNOM valide avec 3 caractère", () => {
-
-
+  it("#2 | Zone PRÉNOM valide avec 3 caractère", () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("a".repeat(3));
+    expect(zone.valid).toBeTruthy();
   });
 
-  // it("#3 | Zone PRÉNOM valide avec 3 caractère", () => {
-  // });
+  // ne fonctionne pas
+  it("#3 | Zone PRÉNOM valide avec 200 caractère", () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("a".repeat(200));
+    expect(zone.valid).toBeTruthy();
+  });
 
   it("#4 | Zone PRÉNOM invalide avec aucune valeur", () => {
+    let zone = component.problemeForm.controls['prenom'];
+    let errors = zone.errors || {};
+    expect(errors['required']).toBeTruthy();
   });
 
   it("#5 | Zone PRÉNOM valide avec 10 espace", () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("          ");
+    expect(zone.valid).toBeTruthy();
   });
 
   it("#6 | Zone PRÉNOM valide avec 2 espace et 1 caractère", () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("  a");
+    expect(zone.valid).toBeTruthy();
   });
 
 });
